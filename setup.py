5c15cd78f4e3f5a0ce63ec71e80e178f91a8f15d
luphord <luphord@protonmail.com>
2018-12-04 07:49:13 +0100

    renamed python package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='australian_housing',
     packages=find_packages(),
     version='0.1.0',
     description='"A data science exercise on Australian house approvements"',
