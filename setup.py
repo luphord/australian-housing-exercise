2c3add669b992e565964e55e733599be6aa3dbb1
luphord <luphord@protonmail.com>
2018-12-04 07:46:52 +0100

    initial commit based on data science cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='"A data science exercise on Australian house approvements"',
+    author='"luphord"',
+    license='MIT',
+)
