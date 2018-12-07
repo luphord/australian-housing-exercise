6008cf81a34b8bf3cf102a2cc9b004bcd3e1ea6b
luphord <luphord@protonmail.com>
2018-12-07 17:24:16 +0100

    fix logging

--- australian_housing/__main__.py
+++ australian_housing/__main__.py
@@ -1,4 +1,8 @@
+import sys
+import logging
 from . import main
 
 if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    logger = logging.getLogger(__name__)
     main()
