464e981533f26f0144ad602b6c0b954ec90415aa
luphord <luphord@protonmail.com>
2019-01-23 20:39:53 +0100

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='australian_housing',
     packages=find_packages(),
-    version='0.2.0',
+    version='0.2.1',
     description='"A data science exercise on Australian house approvements"',
     author='"luphord"',
     license='MIT',
